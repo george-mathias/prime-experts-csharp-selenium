4b41db2716ce4ed9c53e39a6d83b61e30c9e6670
Mathias, George <gmathias80@gmail.com>
2021-06-13 12:27:48 -0300

    aula 01, finalizad, declarando classe e metodo de teste, instalando selenium via nuget, abrindo e fechando o navegador

---
+++ autom-web-com-csharp-e-selenium/01-introducao-ao-csharp-com-selenium/GoogleTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_introducao_ao_csharp_com_selenium
+{
+    [TestClass]
+    public class GoogleTest
+    {
+        [TestMethod]
+        public void TestePesquisar()
+        {
+            ChromeDriver driver = new ChromeDriver();
+
+            try
+            {
+                Assert.Fail();
+            }
+            catch (Exception)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
